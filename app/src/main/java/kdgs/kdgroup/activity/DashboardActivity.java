1ce8ffea8603648855caf7ec46604c174b06257d
Pratik Patel <pratik@adsumoriginator.com>
2019-03-07 20:17:31 +0530

    Updatte with phone call permission and share menu added

--- app/src/main/java/kdgs/kdgroup/activity/DashboardActivity.java
+++ app/src/main/java/kdgs/kdgroup/activity/DashboardActivity.java
@@ -1,19 +1,32 @@
 package kdgs.kdgroup.activity;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 import butterknife.BindView;
 import kdgs.kdgroup.R;
 import kdgs.kdgroup.base.BaseActivity;
 import kdgs.kdgroup.config.CommonFunctions;
+import kdgs.kdgroup.config.Constants;
 
 public class DashboardActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -23,6 +36,7 @@ public class DashboardActivity extends BaseActivity implements NavigationView.On
     Toolbar toolbar;
     @BindView(R.id.drawer_layout)
     DrawerLayout drawer;
+    private PermissionRequestErrorListener errorListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,16 +77,17 @@ public class DashboardActivity extends BaseActivity implements NavigationView.On
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        //getMenuInflater().inflate(R.menu.dashboard, menu);
+        getMenuInflater().inflate(R.menu.menu_share, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        /*if (id == R.id.action_settings) {
+        if (id == R.id.action_share) {
+            shareApp();
             return true;
-        }*/
+        }
         return super.onOptionsItemSelected(item);
     }
 
@@ -90,11 +105,76 @@ public class DashboardActivity extends BaseActivity implements NavigationView.On
 
         } else if (id == R.id.nav_profile) {
 
+        } else if (id == R.id.nav_contact) {
+            callPermissionListeners();
         } else if (id == R.id.nav_logout) {
             startActivity(new Intent(this, SigninActivity.class));
             this.finish();
         }
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void shareApp() {
+        try {
+            String shareMsg = getString(R.string.subject) + " \n ";
+            String play_store = getString(R.string.google_url);
+            String appName = getPackageName();
+
+            Intent linkIntent = new Intent(Intent.ACTION_SEND);
+            linkIntent.setType("text/plain");
+            linkIntent.putExtra(Intent.EXTRA_TEXT, shareMsg + play_store + appName);
+            startActivity(Intent.createChooser(linkIntent, "Share Via"));
+        } catch (android.content.ActivityNotFoundException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void callPermissionListeners() {
+        try {
+            errorListener = new PermissionRequestErrorListener() {
+                @Override
+                public void onError(DexterError error) {
+                }
+            };
+
+            Dexter.withActivity(this)
+                    .withPermission(Manifest.permission.READ_PHONE_STATE)
+                    .withListener(new PermissionListener() {
+                        @Override
+                        public void onPermissionGranted(PermissionGrantedResponse response) {
+                        }
+
+                        @Override
+                        public void onPermissionDenied(PermissionDeniedResponse response) {
+                            storagePermissionDialog();
+                        }
+
+                        @Override
+                        public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                            token.continuePermissionRequest();
+                        }
+                    }).check();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void storagePermissionDialog() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setMessage(getString(R.string.storage_permission1));
+        dialogBuilder.setPositiveButton(getString(R.string.dialog_ok_btn), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                callPermissionListeners();
+                dialog.dismiss();
+            }
+        });
+        dialogBuilder.setNegativeButton(getString(R.string.dialog_cancel_btn), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        dialogBuilder.setCancelable(false);
+        dialogBuilder.show();
     }
 }
--- app/src/main/java/kdgs/kdgroup/config/Constants.java
+++ app/src/main/java/kdgs/kdgroup/config/Constants.java
@@ -2,4 +2,6 @@ package kdgs.kdgroup.config;
 
 public class Constants {
     public static final String HTTP_ATH ="ATH" ;
+
+    public static String callPermission = "callPermission";
 }
