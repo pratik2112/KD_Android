072cebe90d176257f9ac35b2ba4251fcf351df0d
Pratik Patel <pratik@adsumoriginator.com>
2019-03-08 14:52:20 +0530

    Update with register and reset password screen.

--- app/src/main/java/kdgs/kdgroup/activity/ForgotActivity.java
+++ app/src/main/java/kdgs/kdgroup/activity/ForgotActivity.java
@@ -2,13 +2,23 @@ package kdgs.kdgroup.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
+import butterknife.BindView;
 import butterknife.OnClick;
 import kdgs.kdgroup.R;
 import kdgs.kdgroup.base.BaseActivity;
 import kdgs.kdgroup.config.CommonFunctions;
 
 public class ForgotActivity extends BaseActivity {
+
+    @BindView(R.id.rl_send_otp)
+    RelativeLayout rl_send_otp;
+    @BindView(R.id.rl_verify_otp)
+    RelativeLayout rl_verify_otp;
+    @BindView(R.id.rl_reset_pwd)
+    RelativeLayout rl_reset_pwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +40,26 @@ public class ForgotActivity extends BaseActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @OnClick(R.id.btn_send_otp)
+    public void sendOTPClick() {
+        rl_send_otp.setVisibility(View.GONE);
+        rl_verify_otp.setVisibility(View.VISIBLE);
+        rl_reset_pwd.setVisibility(View.GONE);
+    }
+
+    @OnClick(R.id.btn_verify)
+    public void verifyClick() {
+        rl_send_otp.setVisibility(View.GONE);
+        rl_verify_otp.setVisibility(View.GONE);
+        rl_reset_pwd.setVisibility(View.VISIBLE);
+    }
+
+    @OnClick(R.id.btn_submit)
+    public void submitClick() {
+        startActivity(new Intent(ForgotActivity.this, SigninActivity.class));
+        finish();
     }
 
     @OnClick(R.id.tv_signin)
--- app/src/main/java/kdgs/kdgroup/activity/SignupActivity.java
+++ app/src/main/java/kdgs/kdgroup/activity/SignupActivity.java
@@ -1,14 +1,37 @@
 package kdgs.kdgroup.activity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import butterknife.BindView;
 import butterknife.OnClick;
 import kdgs.kdgroup.R;
 import kdgs.kdgroup.base.BaseActivity;
 import kdgs.kdgroup.config.CommonFunctions;
 
 public class SignupActivity extends BaseActivity {
+
+    @BindView(R.id.rl_step1)
+    RelativeLayout rl_step1;
+    @BindView(R.id.rl_step2)
+    RelativeLayout rl_step2;
+    @BindView(R.id.rl_step3)
+    RelativeLayout rl_step3;
+    @BindView(R.id.tv_note)
+    TextView tv_note;
+    @BindView(R.id.edt_email)
+    EditText edt_email;
+    @BindView(R.id.ll_email_confirm)
+    LinearLayout ll_email_confirm;
+    @BindView(R.id.ll_signup)
+    LinearLayout ll_signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +49,39 @@ public class SignupActivity extends BaseActivity {
             getSupportActionBar().hide();
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             if (CommonFunctions.checkConnection(this)) {
+
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    @OnClick(R.id.tv_signin)
+    @OnClick(R.id.btn_submit)
+    public void submitClick() {
+        rl_step1.setVisibility(View.GONE);
+        rl_step2.setVisibility(View.VISIBLE);
+        rl_step3.setVisibility(View.GONE);
+    }
+
+    @OnClick(R.id.btn_verify)
+    public void verifyClick() {
+        rl_step1.setVisibility(View.GONE);
+        rl_step2.setVisibility(View.GONE);
+        rl_step3.setVisibility(View.VISIBLE);
+    }
+
+    @OnClick(R.id.btn_submit1)
+    public void acuntSubmitClick() {
+        try {
+            ll_signup.setVisibility(View.GONE);
+            ll_email_confirm.setVisibility(View.VISIBLE);
+            tv_note.setText(Html.fromHtml(getString(R.string.str_note, edt_email.getText().toString())));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @OnClick({R.id.tv_signin, R.id.btn_ok})
     public void signinClick() {
         startActivity(new Intent(SignupActivity.this, SigninActivity.class));
         finish();
